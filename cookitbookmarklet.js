0bbf9f543e4bf3cb2e7c436a7179e92d9b476119
Alex Feldman <alexandramfeldman@gmail.com>
2013-11-07 18:40:32 -0800

    modified javascript: slightly...

--- cookitbookmarklet.js
+++ cookitbookmarklet.js
@@ -1,4 +1,13 @@
+var _cookit_css = document.createElement('LINK');
+_cookit_css.rel='stylesheet';
+_cookit_css.type='text/css';
+_cookit_css.href='http://localhost:8000/dropbox/dropboxapiapp/cookitstyle.css';
+document.getElementsByTagName('head')[0].appendChild(_cookit_css);
 
-window.onload = function () {
-    alert("successfully did stuff!");
-}();
+
+var _cookit_div = document.createElement('DIV');
+_cookit_div.id='singleCookitDiv';
+document.getElementsByTagName('body')[0].appendChild(_cookit_div);
+
+//_cookit_div.createTextNode("hello there, and greetings.");
+
